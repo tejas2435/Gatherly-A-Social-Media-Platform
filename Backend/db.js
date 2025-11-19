9d59681b8722a411497b6897639bc9a1f1afc1ca
TEJAS MISTRY <160476195+tejas2435@users.noreply.github.com>
2025-11-19 20:16:53 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -4,12 +4,11 @@ const { Pool } = pkg;
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'social_auth',
-  // password: 'Dhyana@123',
-  // password: 'qwerty',
-  password: '123456123',
+  database: 'social_auth', // here enter your database name 
+  password: '123456123', // here enter your password for pgadmin 
 
   port: 5432,
 });
 
 export default pool;
+
